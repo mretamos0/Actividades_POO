da66aebb32e3351c18dde9b0f5f9f12f6be3079c
mretamos0 <maxiretamosotrotta@gmail.com>
2025-05-20 22:21:51 -0300

    first commit

---
+++ src/Unidad_4/Herencia_1/Metodo_Pago/MetodoPago.java
@@ -0,0 +1,20 @@
+/* Crear una clase abstracta llamada MetodoPago que contenga un atributo montoBase (double), un método constructor y un método abstracto calcularMontoFinal() que retorne un double.
+Crear tres subclases concretas que extiendan MetodoPago:
+    - PagoConTarjetaCredito: Aplica un recargo del 10% sobre el monto base.
+    - PagoConDebito: No aplica recargo ni descuento.
+    - PagoConTransferencia: Aplica un descuento del 5%.
+Crear una clase UsarMetodosPago en la que se tienen que crear 3 instancias de cada método de pago y se muestren por pantalla los montos finales calculados con cada método. */
+
+package Unidad_4.Herencia_1.Metodo_Pago;
+
+public abstract class MetodoPago {
+    protected double montoBase;
+
+    public MetodoPago() {
+        this.montoBase = 0.0;
+    }
+
+    public abstract double calcularMontoFinal() {
+        
+    }
+}
