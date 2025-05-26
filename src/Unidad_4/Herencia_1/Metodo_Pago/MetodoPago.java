db6f95195b165f4b2d6be2201b5a0459fe764b61
mretamos0 <maxiretamosotrotta@gmail.com>
2025-05-25 23:47:49 -0300

    Ejercicio Metodo pago

--- src/Unidad_4/Herencia_1/Metodo_Pago/MetodoPago.java
+++ src/Unidad_4/Herencia_1/Metodo_Pago/MetodoPago.java
@@ -4,7 +4,6 @@ Crear tres subclases concretas que extiendan MetodoPago:
     - PagoConDebito: No aplica recargo ni descuento.
     - PagoConTransferencia: Aplica un descuento del 5%.
 Crear una clase UsarMetodosPago en la que se tienen que crear 3 instancias de cada método de pago y se muestren por pantalla los montos finales calculados con cada método. */
-
 package Unidad_4.Herencia_1.Metodo_Pago;
 
 public abstract class MetodoPago {
