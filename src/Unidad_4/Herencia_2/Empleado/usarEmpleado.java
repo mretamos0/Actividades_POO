4e544f03046569229b671af093496cebbe568f36
mretamos0 <maxiretamosotrotta@gmail.com>
2025-05-26 01:23:54 -0300

    Ejercicio Empleado

--- src/Unidad_4/Herencia_2/Empleado/usarEmpleado.java
+++ src/Unidad_4/Herencia_2/Empleado/usarEmpleado.java
@@ -3,7 +3,7 @@ package Unidad_4.Herencia_2.Empleado;
 import Unidad_4.Herencia_2.Empleado.tipoEmpleado.EmpleadoConComision;
 import Unidad_4.Herencia_2.Empleado.tipoEmpleado.EmpleadoPorHora;
 
-public class usarEmpleado {
+public class UsarEmpleado {
     public static void main(String[] args) {
         Empleado e1 = new EmpleadoPorHora("Andres", 8, 3000);
         Empleado e2 = new EmpleadoConComision("Carlos", 100, 5, 0.5);
