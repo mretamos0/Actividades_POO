1154c1dbf80f838e1aa47f95a2731e4ee505433e
mretamos0 <maxiretamosotrotta@gmail.com>
2025-05-26 21:49:51 -0300

    Ejercicio Receta

--- src/Unidad_4/Herencia_3/Receta/Cocina.java
+++ src/Unidad_4/Herencia_3/Receta/Cocina.java
@@ -1,10 +1,20 @@
 package Unidad_4.Herencia_3.Receta;
 
+import Unidad_4.Herencia_3.Receta.tipoReceta.RecetaEnsalada;
+import Unidad_4.Herencia_3.Receta.tipoReceta.RecetaPastel;
+
 //En la clase de prueba, agregue un arreglo (no esta en la consigna) para instanciar distintas recetas  
 
 public class Cocina {
     public static void main(String[] args) {
-        Receta[] Recetas = new Recetas[2];
-        
+        Receta[] recetas = new Receta[3];
+
+        recetas[1] = new RecetaPastel("Torta de vainilla");
+        recetas[2] = new RecetaEnsalada("Ensalada de tomate");
+        recetas[3] = new RecetaPastel("Torta de chocolate");
+
+        for(int i = 0; i < recetas.length; i++) {
+            System.out.println("Receta N°" + (i+1) + ": " + recetas[i] + "\nPreparacion: " + recetas[i].preparar());
+        }
     }
 }
