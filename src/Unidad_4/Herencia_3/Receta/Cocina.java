22711661c58b32a01f408c7e8971dc5913f1f8e0
mretamos0 <maxiretamosotrotta@gmail.com>
2025-05-26 21:28:41 -0300

    Ejercicio Receta

---
+++ src/Unidad_4/Herencia_3/Receta/Cocina.java
@@ -0,0 +1,7 @@
+package Unidad_4.Herencia_3.Receta;
+
+//En la clase de prueba, agregue un arreglo para crear instancias de la clase Receta (no esta en la consigna)
+
+public class Cocina {
+    public static
+}
