a2ce28052a38e353dd041f456e7b36b6fca2a8cf
mretamos0 <maxiretamosotrotta@gmail.com>
2025-06-05 04:05:20 -0300

    Ejercicio Promedio

---
+++ src/Unidad_5/Colecciones_2/Enteros/Enteros.java
@@ -0,0 +1,28 @@
+/*Crear un programa que contenga un ArrayList de enteros. Cargar 10 números al azar utilizando Math.random(). 
+Desarrollar una función que calcule el promedio de los mismos y que dicho resultado sea luego mostrado en pantalla desde el programa principal. */
+package Unidad_5.Colecciones_2.Enteros;
+import java.util.ArrayList;
+
+public class Enteros {
+    public static void main(String[] args) {
+        ArrayList <Integer> listaEnteros = new ArrayList<>();
+        int suma = 0;
+
+        for(int i = 0; i < 10; i++) {
+            int numero = (int)(Math.random() * 100) + 1; //Este rango va de 1 a 100
+            listaEnteros.add(numero);
+            suma += numero;
+        }
+        
+        System.out.println("Lista de numeros enteros:");
+        for(int numero : listaEnteros) {
+            System.out.println("-" + numero);
+        }
+        calcularPromedio(listaEnteros, suma);
+    }
+
+    public static void calcularPromedio(ArrayList<Integer> listaEnteros, int suma) {
+        double promedio = (double) suma / listaEnteros.size();
+        System.out.println("\nEl promedio es: " + promedio);
+    } 
+}
